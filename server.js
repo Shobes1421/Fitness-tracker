ebb0bc5d1085f6552b63754e41c90d68be22a1bb
David Shober <shobes1421@hotmail.com>
2020-08-12 22:02:51 -0600

    make it better

--- server.js
+++ server.js
@@ -1,40 +1,33 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const mongojs = require("mongojs");
+const workout = require("./models");
 
-const PORT = process.env.PORT || 3030;
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(logger("dev"));
+
+
+// else {
+    app.use(logger("dev"));
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-const databaseUrl = "workout";
-const collections = ["exercise"];
-
-const db = mongojs(databaseUrl, collections);
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
-
-const MONGOURL = process.env.MONGODB_URI || "mongodb://heroku_bj8917sh:password123@ds023603.mlab.com:23603/heroku_bj8917sh";
-
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(process.env.MONGODB || "mongodb://localhost/workoutdb");
+//mongoose.connect("mongodb://heroku_bj8917sh:password123@ds023603.mlab.com:23603/heroku_bj8917sh")
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`
+Welcome to Workout Tracker!
+running on port ${PORT}.`);
 });
 
+//mongodb://heroku_bj8917sh:password123@ds023603.mlab.com:23603/heroku_bj8917sh
